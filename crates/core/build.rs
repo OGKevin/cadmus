1576823fbc68e0c3bf696c68d6f18f8bd6a39a9a
Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>
2026-01-21 19:16:18 +0100

    fix: add --first-parent to git describe in builds
    In CI, there is a fake merge commit that merges the branch with default
    branch to ensure test pass after merging. This causes `git describe` to
    point to a commit that is not on the current branch, which is not
    desired.
    
    - crates/cadmus/build.rs: add --first-parent flag
    - crates/core/build.rs: add --first-parent flag
    - crates/emulator/build.rs: add --first-parent flag
    - crates/ui-dev/build.rs: add --first-parent flag
    
    Change-Id: 4ec368f05d9923b4511d251a735899b9
    Change-Id-Short: vlnwtrkzumqq

--- crates/core/build.rs
+++ crates/core/build.rs
@@ -7,7 +7,13 @@ fn main() {
     println!("cargo:rerun-if-changed=.git/HEAD");
 
     let git_version = Command::new("git")
-        .args(["describe", "--tags", "--always", "--dirty"])
+        .args([
+            "describe",
+            "--tags",
+            "--always",
+            "--dirty",
+            "--first-parent",
+        ])
         .output()
         .ok()
         .and_then(|output| {
