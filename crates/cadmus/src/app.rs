OGKevin/plato

feat: add settings editor interface
Introduce a new settings editor view that allows users to manage
application settings through a GUI. The editor includes:

- New SettingsEditor view component with category-based organization
- CategoryRow and SettingRow UI components for displaying settings
- CategoryEditor for editing individual settings categories
- SettingValue abstraction for handling different setting types
- Settings saved to disk using TOML format
- Accessible from main menu via new Settings command

Change-Id: 3cfc132b81bbcd5b2bb065aafe19198d
Change-Id-Short: wnknywxoryoo

crates/plato/src/app.rs
@@ -36,6 +36,7 @@ use plato_core::view::menu::{Menu, MenuKind};
 use plato_core::view::notification::Notification;
 use plato_core::view::reader::Reader;
 use plato_core::view::rotation_values::RotationValues;
+use plato_core::view::settings_editor::SettingsEditor;
 use plato_core::view::sketch::Sketch;
 use plato_core::view::touch_events::TouchEvents;
 use plato_core::view::{handle_event, process_render_queue, wait_for_all};
@@ -1077,6 +1078,10 @@ pub fn run() -> Result<(), Error> {
                         &mut rq,
                         &mut context,
                     )),
+                    AppCmd::SettingsEditor => Box::new(
+                        SettingsEditor::new(context.fb.rect(), &tx, &mut rq, &mut context)
+                            .build()?,
+                    ),
                     AppCmd::TouchEvents => {
                         Box::new(TouchEvents::new(context.fb.rect(), &mut rq, &mut context))
                     }

crates/emulator/src/main.rs
@@ -31,6 +31,7 @@ use plato_core::view::menu::{Menu, MenuKind};
 use plato_core::view::notification::Notification;
 use plato_core::view::reader::Reader;
 use plato_core::view::rotation_values::RotationValues;
+use plato_core::view::settings_editor::SettingsEditor;
 use plato_core::view::sketch::Sketch;
 use plato_core::view::touch_events::TouchEvents;
 use plato_core::view::{handle_event, process_render_queue, wait_for_all, RenderData, RenderQueue};
@@ -555,6 +556,10 @@ fn main() -> Result<(), Error> {
                             &mut rq,
                             &mut context,
                         )),
+                        AppCmd::SettingsEditor => Box::new(
+                            SettingsEditor::new(context.fb.rect(), &tx, &mut rq, &mut context)
+                                .build()?,
+                        ),
                         AppCmd::TouchEvents => {
                             Box::new(TouchEvents::new(context.fb.rect(), &mut rq, &mut context))
                         }

OGKevin/cadmus

crates/cadmus/src/app.rs
@@ -38,6 +38,7 @@ use cadmus_core::view::notification::Notification;
 use cadmus_core::view::ota::show_ota_view;
 use cadmus_core::view::reader::Reader;
 use cadmus_core::view::rotation_values::RotationValues;
+use cadmus_core::view::settings_editor::SettingsEditor;
 use cadmus_core::view::sketch::Sketch;
 use cadmus_core::view::touch_events::TouchEvents;
 use cadmus_core::view::{handle_event, process_render_queue, wait_for_all};
@@ -1107,6 +1108,10 @@ pub fn run() -> Result<(), Error> {
                         &mut rq,
                         &mut context,
                     )),
+                    AppCmd::SettingsEditor => Box::new(
+                        SettingsEditor::new(context.fb.rect(), &tx, &mut rq, &mut context)
+                            .build()?,
+                    ),
                 };
                 transfer_notifications(view.as_mut(), next_view.as_mut(), &mut rq, &mut context);
                 history.push(HistoryItem {

crates/emulator/src/main.rs
@@ -31,6 +31,7 @@ use cadmus_core::view::menu::{Menu, MenuKind};
 use cadmus_core::view::notification::Notification;
 use cadmus_core::view::reader::Reader;
 use cadmus_core::view::rotation_values::RotationValues;
+use cadmus_core::view::settings_editor::SettingsEditor;
 use cadmus_core::view::sketch::Sketch;
 use cadmus_core::view::touch_events::TouchEvents;
 use cadmus_core::view::{
@@ -565,6 +566,10 @@ fn main() -> Result<(), Error> {
                             &mut rq,
                             &mut context,
                         )),
+                        AppCmd::SettingsEditor => Box::new(
+                            SettingsEditor::new(context.fb.rect(), &tx, &mut rq, &mut context)
+                                .build()?,
+                        ),
                         AppCmd::TouchEvents => {
                             Box::new(TouchEvents::new(context.fb.rect(), &mut rq, &mut context))
                         }