f889c0d438426ee7dbd27dadc06c02e977405544
Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>
2026-02-06 11:18:10 +0100

    feat: add emulator feature flag
    Add an emulator feature flag to change behaviour when the emulator is
    running.
    
    Change-Id: ba1bf29816c73b5ccff29ee2896dbb1a
    Change-Id-Short: opyokxqrytns

--- devenv.nix
+++ devenv.nix
@@ -588,7 +588,7 @@ in
       export OTEL_EXPORTER_OTLP_ENDPOINT="http://localhost:4318"
       export RUST_LOG="trace"
 
-      ./run-emulator.sh --features otel "$@"
+      ./run-emulator.sh --features otel,test,emulator "$@"
     '';
   };
 
