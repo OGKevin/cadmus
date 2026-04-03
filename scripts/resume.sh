7d321d102df89d8c5797f7e7bb081253dda531e6
Kevin <17928966+OGKevin@users.noreply.github.com>
2026-04-03 19:12:24 +0200

    Apply suggestion from @github-actions[bot]
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- scripts/resume.sh
+++ scripts/resume.sh
@@ -2,7 +2,7 @@
 
 echo 0 > /sys/power/state-extended
 case "$PRODUCT" in
-	alyssum|dahlia)
-		echo a > /sys/devices/virtual/input/input1/neocmd
-		;;
+  alyssum | dahlia)
+    echo a >/sys/devices/virtual/input/input1/neocmd
+    ;;
 esac
