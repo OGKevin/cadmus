6cc65b5ba99c841085b958b6ef066d4af7287ce0
Nishant Rodrigues <nishantjr@gmail.com>
2026-08-17 12:57:43 +0530

    feat(Frontlight): increment, decrement buttons for finegrain control (#706)

--- crates/core/src/view/frontlight.rs
+++ crates/core/src/view/frontlight.rs
@@ -3,7 +3,7 @@ use super::common::shift;
 use super::icon::Icon;
 use super::label::Label;
 use super::presets_list::PresetsList;
-use super::slider::Slider;
+use super::slider::SliderWithButtons;
 use super::{
     Align, Bus, EntryId, Event, Hub, ID_FEEDER, Id, RenderData, RenderQueue, SliderId, View, ViewId,
 };
@@ -137,7 +137,7 @@ impl FrontlightWindow {
                     levels.warmth
                 };
 
-                let slider = Slider::new(
+                let slider = SliderWithButtons::new(
                     rect![
                         rect.min.x + max_label_width + 3 * padding,
                         min_y,
@@ -155,7 +155,7 @@ impl FrontlightWindow {
             button_y += small_height;
         } else {
             let min_y = rect.min.y + small_height;
-            let slider = Slider::new(
+            let slider = SliderWithButtons::new(
                 rect![
                     rect.min.x + padding,
                     min_y,
@@ -275,14 +275,15 @@ impl FrontlightWindow {
         self.frontlight_levels = frontlight_levels;
         let LightLevels { intensity, warmth } = frontlight_levels;
         if context.device.has_natural_light() {
-            if let Some(slider_intensity) = self.child_mut(3).downcast_mut::<Slider>() {
-                slider_intensity.update(intensity.into(), rq);
+            if let Some(slider_intensity) = self.child_mut(3).downcast_mut::<SliderWithButtons>() {
+                slider_intensity.slider().update(intensity.into(), rq);
             }
-            if let Some(slider_warmth) = self.child_mut(5).downcast_mut::<Slider>() {
-                slider_warmth.update(warmth.into(), rq);
+            if let Some(slider_warmth) = self.child_mut(5).downcast_mut::<SliderWithButtons>() {
+                slider_warmth.slider().update(warmth.into(), rq);
             }
-        } else if let Some(slider_intensity) = self.child_mut(2).downcast_mut::<Slider>() {
-            slider_intensity.update(intensity.into(), rq);
+        } else if let Some(slider_intensity) = self.child_mut(2).downcast_mut::<SliderWithButtons>()
+        {
+            slider_intensity.slider().update(intensity.into(), rq);
         }
     }
 
--- crates/core/src/view/slider.rs
+++ crates/core/src/view/slider.rs
@@ -8,6 +8,7 @@ use crate::framebuffer::UpdateMode;
 use crate::geom::{BorderSpec, CornerSpec, Rectangle, halves};
 use crate::input::{DeviceEvent, FingerStatus};
 use crate::unit::scale_by_dpi;
+use crate::view::icon::Icon;
 
 const PROGRESS_HEIGHT: f32 = 7.0;
 const BUTTON_DIAMETER: f32 = 46.0;
@@ -45,7 +46,21 @@ impl Slider {
         }
     }
 
-    pub fn update_value(&mut self, x_hit: i32, dpi: u16) {
+    /// Increment (or decrement for negative amounts) the slider's value.
+    pub fn increment(&mut self, amount: f32, rq: &mut RenderQueue) {
+        self.update(self.value + amount, rq);
+    }
+
+    /// Update the value of the slider, taking into account the bounds.
+    pub fn update(&mut self, value: f32, rq: &mut RenderQueue) {
+        if (self.value - value).abs() >= f32::EPSILON {
+            self.value = f32::max(f32::min(value, self.max_value), self.min_value);
+            rq.add(RenderData::new(self.id, self.rect, UpdateMode::Gui));
+        }
+    }
+
+    /// Update the slider given an x co-ordinate.
+    fn update_value(&mut self, x_hit: i32, dpi: u16) {
         let button_diameter = scale_by_dpi(BUTTON_DIAMETER, dpi) as i32;
         let (small_radius, big_radius) = halves(button_diameter);
         let x_offset = x_hit
@@ -55,13 +70,6 @@ impl Slider {
             / (self.rect.width() as i32 - button_diameter) as f32)
             .clamp(0.0, 1.0);
         self.value = self.min_value + progress * (self.max_value - self.min_value);
-    }
-
-    pub fn update(&mut self, value: f32, rq: &mut RenderQueue) {
-        if (self.value - value).abs() >= f32::EPSILON {
-            self.value = value;
-            rq.add(RenderData::new(self.id, self.rect, UpdateMode::Gui));
-        }
     }
 }
 
@@ -83,7 +91,7 @@ impl View for Slider {
                 FingerStatus::Down if self.rect.includes(position) => {
                     self.active = true;
                     self.update_value(position.x, _context.device.dpi());
-                    rq.add(RenderData::new(self.id, self.rect, UpdateMode::Gui));
+                    rq.add(RenderData::new(self.id, self.rect, UpdateMode::FastMono));
                     bus.push_back(Event::Slider(self.slider_id, self.value, status));
                     self.last_x = position.x;
                     true
@@ -210,5 +218,191 @@ impl View for Slider {
 
     fn id(&self) -> Id {
         self.id
+    }
+}
+
+/// A slider view with increment and decrement buttons.
+pub struct SliderWithButtons {
+    id: Id,
+    rect: Rectangle,
+    children: Vec<Box<dyn View>>,
+    #[cfg(test)]
+    decrement_index: usize,
+    slider_index: usize,
+}
+
+impl SliderWithButtons {
+    pub fn new(
+        rect: Rectangle,
+        slider_id: SliderId,
+        value: f32,
+        min_value: f32,
+        max_value: f32,
+    ) -> SliderWithButtons {
+        let decrement = Icon::new(
+            "minus",
+            rect![rect.min.x, rect.min.y, rect.min.x + 40, rect.max.y],
+            Event::SliderIncrement(-1.0),
+        );
+        let slider = Slider::new(
+            rect![
+                decrement.rect.max.x,
+                rect.min.y,
+                rect.max.x - 40,
+                rect.max.y
+            ],
+            slider_id,
+            value,
+            min_value,
+            max_value,
+        );
+
+        let increment = Icon::new(
+            "plus",
+            rect![slider.rect.max.x, rect.min.y, rect.max.x, rect.max.y],
+            Event::SliderIncrement(1.0),
+        );
+
+        let mut children: Vec<Box<dyn View>> = vec![];
+        #[cfg(test)]
+        let decrement_index = children.len();
+        children.push(Box::new(decrement));
+        let slider_index = children.len();
+        children.push(Box::new(slider));
+        children.push(Box::new(increment));
+        SliderWithButtons {
+            rect,
+            id: ID_FEEDER.next(),
+            children,
+            #[cfg(test)]
+            decrement_index,
+            slider_index,
+        }
+    }
+
+    pub fn slider(&mut self) -> &mut Slider {
+        self.child_mut(self.slider_index)
+            .downcast_mut::<Slider>()
+            .unwrap()
+    }
+}
+
+impl View for SliderWithButtons {
+    #[cfg_attr(feature = "tracing", tracing::instrument(skip(self, _hub, bus, rq, _context), fields(event = ?evt
+    ), ret(level=tracing::Level::TRACE)))]
+    fn handle_event(
+        &mut self,
+        evt: &Event,
+        _hub: &Hub,
+        bus: &mut Bus,
+        rq: &mut RenderQueue,
+        _context: &mut AppContext,
+    ) -> bool {
+        match *evt {
+            Event::SliderIncrement(amount) => {
+                let id = self.id;
+                let rect = self.rect;
+                self.slider().increment(amount, rq);
+                rq.add(RenderData::new(id, rect, UpdateMode::Gui));
+                bus.push_back(Event::Slider(
+                    self.slider().slider_id,
+                    self.slider().value,
+                    FingerStatus::Up,
+                ));
+                true
+            }
+            _ => false,
+        }
+    }
+
+    #[cfg_attr(feature = "tracing", tracing::instrument(skip(self, _rect, _context), fields(rect = ?_rect)))]
+    fn render(&self, _context: &mut AppContext, _rect: Rectangle) {}
+
+    fn rect(&self) -> &Rectangle {
+        &self.rect
+    }
+
+    fn rect_mut(&mut self) -> &mut Rectangle {
+        &mut self.rect
+    }
+    fn children(&self) -> &Vec<Box<dyn View>> {
+        &self.children
+    }
+    fn children_mut(&mut self) -> &mut Vec<Box<dyn View>> {
+        &mut self.children
+    }
+    fn id(&self) -> Id {
+        self.id
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use crate::context::test_helpers::create_test_context;
+    use crate::geom::Point;
+    use crate::gesture::GestureEvent;
+    use std::collections::VecDeque;
+    use std::sync::mpsc::channel;
+
+    #[test]
+    fn test_slider_cannot_update_above_max() {
+        let bounds = rect![0, 0, 200, 50];
+        let mut slider = Slider::new(bounds, SliderId::LightIntensity, 7.0, 5.0, 10.0);
+        let mut rq = RenderQueue::new();
+
+        slider.update(100.0, &mut rq);
+        assert_eq!(slider.value, slider.max_value);
+
+        slider.update(0.0, &mut rq);
+        assert_eq!(slider.value, slider.min_value);
+    }
+
+    #[test]
+    fn test_tap_decrements_value_and_emits_event() {
+        let bounds = rect![0, 0, 200, 50];
+        let mut slider = SliderWithButtons::new(bounds, SliderId::LightIntensity, 7.0, 5.0, 6.0);
+
+        let (hub, _receiver) = channel();
+        let mut bus = VecDeque::new();
+        let mut rq = RenderQueue::new();
+        let mut context = create_test_context();
+
+        let dec_bounds = slider.child(slider.decrement_index).rect();
+        let point = Point::new(
+            dec_bounds.min.x + dec_bounds.max.x / 2,
+            dec_bounds.min.y + dec_bounds.max.y / 2,
+        );
+        let tap_event = Event::Gesture(GestureEvent::Tap(point));
+        crate::view::handle_event(
+            slider.child_mut(slider.decrement_index),
+            &tap_event,
+            &hub,
+            &mut bus,
+            &mut rq,
+            &mut context,
+        );
+        assert_eq!(bus.len(), 1);
+        let increment_event = bus.pop_front().unwrap();
+
+        crate::view::handle_event(
+            &mut slider,
+            &increment_event,
+            &hub,
+            &mut bus,
+            &mut rq,
+            &mut context,
+        );
+        assert_eq!(bus.len(), 1);
+        let update_event = bus.pop_front();
+        assert!(matches!(
+            update_event,
+            Some(Event::Slider(
+                SliderId::LightIntensity,
+                6.0,
+                FingerStatus::Up
+            ))
+        ));
     }
 }
